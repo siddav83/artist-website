053b6b04f3b6a6162098d650968348d7773a29ad
aimyv <av533@bath.ac.uk>
2022-10-31 16:38:20 +0000

    added albums

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import TrainerCard from './components/TrainerCard';
 import PokemonCards from './components/PokemonCards';
 import Artist from './components/Artist';
+import Albums from './components/Albums';
 
 function App() {
   return (
     <main>
-      {/* <h1>Lana Del Rey</h1> */}
+      <h1>Lana Del Rey</h1>
       < Artist />
+      < Albums />
       {/* <TrainerCard /> */}
       {/* <PokemonCards /> */}
     </main>
