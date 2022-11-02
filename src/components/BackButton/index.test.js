26969bdced3e6b4747cb5918e5ee9f91d50e355c
aimyv <av533@bath.ac.uk>
2022-11-02 16:01:41 +0000

    new tests work

---
+++ src/components/BackButton/index.test.js
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import BackButton from '.';
+
+describe('back button', () => {
+    beforeEach(() => render(
+        <MemoryRouter>
+            <BackButton/>
+        </MemoryRouter>
+    ))
+
+    test('render back button', () => {
+        const btn = screen.getByRole('button', {name: "BackButton"})
+        expect(btn).toBeInTheDocument
+    })
+})
