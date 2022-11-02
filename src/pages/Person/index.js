0099a64f0df096e1f5673bed78f044f3b98c8e94
aimyv <av533@bath.ac.uk>
2022-11-02 12:01:01 +0000

    used params for lana

--- src/pages/Person/index.js
+++ src/pages/Person/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Artist, Albums } from '../../components';
+import { useParams } from 'react-router-dom';
 
-export default function Lana() {
+export default function Person() {
     return (
         <>
+
+        <h4>{useParams().name}</h4>
+
         < Artist name={'Lana Del Rey'} description={'Elizabeth Woolridge Grant (born June 21, 1985), known professionally as Lana Del Rey, is an American singer-songwriter. Her music is noted for its cinematic quality and exploration of tragic romance, glamour, and melancholia, containing references to contemporary pop culture and 1950s-1960s Americana.'}/>
 
         <Albums album={[
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,4 @@ export { default as Artists } from './Artists'
 export { default as Home } from './Home'
 export { default as NotFound } from './NotFound'
 
-export { default as Lana } from './Lana'
+export { default as Person } from './Person'
